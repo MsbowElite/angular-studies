7ce7c32e1481ab23aeb9fa7757bc49006adbb6f6
LucasRSena <msbowelite@gmail.com>
2025-11-12 14:11:38 -0300

    nested modules imports

--- users-tasks-modular/src/app/app.module.ts
+++ users-tasks-modular/src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent
+  ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
+  //For hybrid approach with standalone components attach in imports instead of declarations
+  imports: [BrowserModule, SharedModule, TasksModule],
 })
 export class AppModule {}
