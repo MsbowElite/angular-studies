649bb0b842a3d3348f00e434a6e3efe914001b80
LucasRSena <msbowelite@gmail.com>
2025-11-12 12:36:37 -0300

    use simple local storage

--- users-tasks/src/app/tasks/tasks.service.ts
+++ users-tasks/src/app/tasks/tasks.service.ts
@@ -29,6 +29,13 @@ export class TasksSerivice {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -42,9 +49,15 @@ export class TasksSerivice {
       dueDate: taskData.dueDate,
     };
     this.tasks.push(newTask);
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
