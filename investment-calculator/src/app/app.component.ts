5b2559e585536c82178a93ca8350602b2787459e
LucasRSena <msbowelite@gmail.com>
2025-11-18 23:54:36 -0300

    events

--- investment-calculator/src/app/app.component.ts
+++ investment-calculator/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
+
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
+
+import type { InvestmentInput } from './investment-input.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserInputComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  onCalculateInvestmentResults(data: InvestmentInput) {
+    
+    const annualData = [];
+    let investmentValue = data.initialInvestment;
+
+    for (let i = 0; i < data.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (data.expectedReturn / 100);
+      investmentValue += interestEarnedInYear + data.annualInvestment;
+      const totalInterest =
+        investmentValue - data.annualInvestment * year - data.initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: data.annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: data.initialInvestment + data.annualInvestment * year,
+      });
+    }
+
+    console.log(annualData);
+  }
+}
